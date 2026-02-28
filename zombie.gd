3e0b80da11369b87025ece100bedc974faba6a8c
grnlizard <Daniel@tazusa.com>
2026-02-28 14:18:42 -0500

    update

--- zombie.gd
+++ zombie.gd
@@ -4,4 +4,9 @@ extends MeleeEnemy
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is PlayerCharacter:
 		body.health = body.health - 1
+	
+		$Area2D.set_deferred('monitoring',false)
+		await get_tree().create_timer(2).timeout
+		$Area2D.set_deferred('monitoring',true)
+		
 	print(body)
