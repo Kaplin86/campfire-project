5c1c0ee15c9471465603b042441767127731b03a
llama <lincoln@tazusa.com>
2026-02-28 10:43:29 -0800

    player

--- playerMovement.gd
+++ playerMovement.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+var health = 10
+
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -8,6 +10,7 @@ func hui():
 	pass
 
 func _physics_process(delta: float) -> void:
+	$TextureProgressBar.value = health
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
